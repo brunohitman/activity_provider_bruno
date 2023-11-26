90417fbce2708b90487263f536e556b70b044d92
brunohitman <69773007+brunohitman@users.noreply.github.com>
2023-11-26 18:51:05 +0000

    Update activity_provider_python.py

--- activity_provider_python.py
+++ activity_provider_python.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory
 from GET import get_example  # Importa a função get_example do GET.py
 from POST import post_example  # Importa a função post_example do POST.py
 
@@ -7,7 +7,15 @@ app = Flask(__name__)
 # Rota para a página inicial
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+# Rota para trazer o JSON de config_param.json
+@app.route('/json-params-atividade')
+def json_params_atividade():
+    try:
+        return send_from_directory('params', 'config_param.json', as_attachment=True)
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': 'Erro ao obter o JSON', 'error': str(e)}), 500
 
 # Rota dinâmica que chama a função do GET.py ou do POST.py
 @app.route('/<path:path>', methods=['GET', 'POST'])
