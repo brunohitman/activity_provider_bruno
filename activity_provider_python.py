65f68db1b3f7aee17ba07dde39b63fce0cc9db9d
brunohitman <69773007+brunohitman@users.noreply.github.com>
2023-11-26 17:23:37 +0000

    Update activity_provider_python.py

--- activity_provider_python.py
+++ activity_provider_python.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from GET import get_example  # Importa a função get_example do GET.py
+from POST import post_example  # Importa a função post_example do POST.py
 
 app = Flask(__name__)
 
@@ -8,10 +9,12 @@ def hello_world():
     return 'Hello, World!'
 
 # Rota dinâmica que chama a função do GET.py
-@app.route('/<path:path>', methods=['GET'])
+@app.route('/<path:path>', methods=['GET', 'POST'])
 def dynamic_route(path):
     if path == 'GET':
         return get_example()
+    elif path == 'POST':
+        return post_example()
     else:
         return jsonify({'status': 'error', 'message': 'Rota não encontrada'}), 404
 
