7dc9486dea200594feb956c2fdc07651aca85c78
brunohitman <69773007+brunohitman@users.noreply.github.com>
2023-11-26 17:14:28 +0000

    Update GET.py

--- GET.py
+++ GET.py
@@ -1,9 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import jsonify
 
-app = Flask(__name__)
-
-# Rota para requisições GET
-@app.route('https://activity-provider-bruno.onrender.com', methods=['GET'])
+# Função para lidar com requisições GET
 def get_example():
     try:
         # Lógica de processamento para requisições GET
@@ -16,7 +13,3 @@ def get_example():
         error_message = str(e)
         result = {'status': 'error', 'message': 'Erro ao processar a requisição', 'error': error_message}
         return jsonify(result), 500
-
-if __name__ == '__main__':
-    # O host '0.0.0.0' significa que o servidor estará acessível externamente
-    app.run(host='0.0.0.0', port=8080)
