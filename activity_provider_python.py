0f096bfd7c01ab4034ccc8dffa1461e53b5fb8eb
brunohitman <69773007+brunohitman@users.noreply.github.com>
2023-11-26 15:41:15 +0000

    Add files via upload

--- activity_provider_python.py
+++ activity_provider_python.py
@@ -1,32 +1,6 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import sys
+from flask import Flask
+app = Flask(__name__)
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Este é um exemplo de um servidor web .</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <port>")
-        sys.exit(1)
-
-    port = int(sys.argv[1])
-
-    webServer = HTTPServer(("localhost", port), MyServer)
-    print(f"Server started http://localhost:{port}")
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
