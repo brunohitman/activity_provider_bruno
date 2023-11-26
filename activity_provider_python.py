1ef9a10296458ce60eb274072c23f6eeb2579761
brunohitman <69773007+brunohitman@users.noreply.github.com>
2023-11-26 18:31:00 +0000

    Add files via upload

--- activity_provider_python.py
+++ activity_provider_python.py
@@ -1,20 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
 from GET import get_example  # Importa a função get_example do GET.py
 from POST import post_example  # Importa a função post_example do POST.py
 
 app = Flask(__name__)
 
+# Rota para a página inicial
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
 
-# Rota dinâmica que chama a função do GET.py
+# Rota dinâmica que chama a função do GET.py ou do POST.py
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def dynamic_route(path):
     if path == 'GET':
         return get_example()
     elif path == 'POST':
         return post_example()
+    elif path == 'config_url.html':
+        return render_template('config_url.html')
     else:
         return jsonify({'status': 'error', 'message': 'Rota não encontrada'}), 404
 
