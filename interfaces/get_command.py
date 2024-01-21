edc23b05326b618b0f1ad4517ef02ecaa29ad618
brunohitman <69773007+brunohitman@users.noreply.github.com>
2024-01-21 16:01:25 +0000

    Create get_command.py

---
+++ interfaces/get_command.py
@@ -0,0 +1,20 @@
+# get_command.py
+from interfaces.command_interface import Command
+from flask import jsonify
+import json
+
+class GetCommand(Command):
+    def execute(self):
+        try:
+            # Lógica de processamento para requisições GET
+            with open('dados_analytics.json') as json_file:
+                data = json.load(json_file)
+                result = {'status': 'success', 'message': 'Requisição GET bem-sucedida!', 'data': data}
+            
+            # Retorna a resposta como JSON
+            return jsonify(result), 200
+        except Exception as e:
+            # Em caso de erro, retorna uma resposta de erro
+            error_message = str(e)
+            result = {'status': 'error', 'message': 'Erro ao processar a requisição', 'error': error_message}
+            return jsonify(result), 500
