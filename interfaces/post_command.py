ee028c7385356e86fb66601a186b61666df462d9
brunohitman <69773007+brunohitman@users.noreply.github.com>
2024-01-21 17:14:22 +0000

    Update post_command.py

--- interfaces/post_command.py
+++ interfaces/post_command.py
@@ -7,7 +7,7 @@ class PostCommand(Command):
     def execute(self):
         try:
             # Verificar e criar tabela se não existir
-            create_table_if_not_exists()
+            self.create_table_if_not_exists()
             
             # Receber dados do corpo da requisição POST
             data = request.json
@@ -39,3 +39,29 @@ class PostCommand(Command):
             error_message = str(e)
             result = {'status': 'error', 'message': 'Erro ao processar a requisição', 'error': error_message}
             return jsonify(result), 500
+
+    def create_table_if_not_exists(self):
+        # Conectar ao banco de dados
+        conn = psycopg2.connect("postgres://db_activity_provider_user:L8hE1UxGVmADUsMARVKDB4CXoFdKMzu9@dpg-cmcsdq021fec73cub2f0-a.oregon-postgres.render.com/db_activity_provider")
+        
+        # Criar um cursor para executar comandos SQL
+        cursor = conn.cursor()
+        
+        # Comando SQL para criar tabela
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS Relatorios_Analiticos_Recebidos (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            type TEXT NOT NULL
+        );
+        """
+        
+        # Executar o comando SQL
+        cursor.execute(create_table_query)
+        
+        # Confirmar a execução do comando
+        conn.commit()
+        
+        # Fechar o cursor e a conexão
+        cursor.close()
+        conn.close()
